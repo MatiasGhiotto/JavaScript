b53c305b54a9b0f954b80cedcdcb6d47eaf22382
matiasghiotto <matiasp.ghiotto@gmail.com>
2022-08-02 22:28:59 -0300

    Entrega1

--- java.js
+++ java.js
@@ -1,10 +1,11 @@
 let cantidad = prompt("Cantidad que desea comprar?");
-let precio = 700;
+let precio = 40;
 let total = cantidad * precio;
 console.log("Total a pagar " + total +" pesos ");
 
-if(total>= 1500){
+if (total>= 200){
     alert("No tiene cargo en el envio!")
-}else if (total < 1500){
+}else if (total <200){
     alert("Cuenta con recargo en el envio!")
 }
+
